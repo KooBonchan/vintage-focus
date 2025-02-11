302cc7c0c350481eee03270656ea35e2b7bf3084
KooBonchan <garam9koo@gmail.com>
2025-02-11 20:08:35 +0900

    basic environment setting: Spring WebFlux + Vite

---
+++ backend/src/main/java/com/dodream/junggo/JunggoApplication.java
@@ -0,0 +1,13 @@
+package com.dodream.junggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunggoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dodream/junggo/JunggoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodream.junggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

29e4c4908985957a610eb67ac4fd40b440773a01
KooBonchan <garam9koo@gmail.com>
2025-02-12 22:01:16 +0900

    Header minimum

--- frontend/src/layouts/HeaderFooterLayout.tsx
+++ frontend/src/layouts/HeaderFooterLayout.tsx
@@ -1,27 +1,49 @@
-import { Outlet } from "react-router";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Outlet, useNavigate } from "react-router";
+import styles from './HeaderFooterLayout.module.css'
 
 export function HeaderFooterLayout() {
-    return (
-      <>
+  return (
+    <>
       <Header></Header>
       <Outlet />
       <Footer></Footer>
-      </>
-    );
-  }
-  
-  function Header(){
-    return (
-    <header>
-    헤더
-    </header>
-    );
-  }
-  
-  function Footer(){
-    return (
+    </>
+  );
+}
+
+function Header() {
+  let navigate = useNavigate();
+
+  return (
+    <Navbar className={styles.nav}>
+      <Container>
+        <Navbar.Brand className={styles.logo}>TEST</Navbar.Brand>
+        <Nav>
+          <Nav.Link onClick={() => { navigate('/') }}
+            style={{ color: '#ccc' }}>
+            Home
+          </Nav.Link>
+          <Nav.Link onClick={() => { navigate('/product') }}
+            style={{ color: '#ccc' }}>
+            Products
+          </Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link onClick={() => { navigate('/login') }}
+            style={{ color: '#ccc' }}>
+            Login
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+function Footer() {
+  return (
     <footer>
-    푸터
+      푸터
     </footer>
-    );
-  }
+  );
+}

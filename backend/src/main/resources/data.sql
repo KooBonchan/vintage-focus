9befa3644e8ee3fcebe3cc51b67c05297da0f094
KooBonchan <garam9koo@gmail.com>
2025-02-19 09:54:35 +0900

    product deserialization error solved

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,7 @@
-MERGE INTO product (id, name, price) KEY (id) VALUES (1, '연습용 Dame 4현', 123456);
-MERGE INTO product (id, name, price) KEY (id) VALUES (2, '실전용 Fender Fretless 5현', 1234567);
+INSERT INTO product (id, name, price)
+SELECT 1, '연습용 Dame 4현', 123456
+WHERE NOT EXISTS (SELECT 1 FROM product WHERE id = 1);
+
+INSERT INTO product (id, name, price)
+SELECT 2, '실전용 Fender 5현', 1234567
+WHERE NOT EXISTS (SELECT 1 FROM product WHERE id = 2);

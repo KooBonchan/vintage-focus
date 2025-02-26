d6bae6a644640c513b53998d215ada81897cfef3
KooBonchan <garam9koo@gmail.com>
2025-02-26 20:47:42 +0900

    product service

--- backend/src/main/java/com/dodream/vintageFocus/config/SwaggerConfig.java
+++ backend/src/main/java/com/dodream/vintageFocus/config/SwaggerConfig.java
@@ -1,9 +1,16 @@
 package com.dodream.vintageFocus.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(
+  info = @Info(
+    title= "Vintage Focus Backend",
+    version = "0.0.1",
+    description = "Documentation APIs"
+  )
+)
 public class SwaggerConfig {
 }
--- backend/src/main/java/com/dodream/vintageFocus/service/ProductService.java
+++ backend/src/main/java/com/dodream/vintageFocus/service/ProductService.java
@@ -1,8 +1,13 @@
 package com.dodream.vintageFocus.service;
 
+import com.dodream.vintageFocus.dto.ProductDTO;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 public interface ProductService {
-
+  Mono<ProductDTO> getProductById(Long id);
+  Mono<ProductDTO> getProductByModelName(String ModelName);
+  Mono<ProductDTO> incrementViewCount(Long id);
+  Mono<ProductDTO> incrementLikeCount(Long id);
 
 }

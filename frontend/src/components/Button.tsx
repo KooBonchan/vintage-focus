1f76c461dd57526eb7e87682fa9098caf7ab0ef5
yk0988 <yk051016@gmail.com>
2025-03-04 17:34:57 +0900

    버튼 컴포넌트 오류 수정

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -3,13 +3,12 @@ import styles from './button.module.css';
 export interface ButtonProps {
   primary?: boolean;
   backgroundColor?: string;
-  size?: 'small' | 'medium' | 'large' | 'biglarge' | 'kingbiglarge' |
+  size?: 'small' | 'medium' | 'large' | 'biglarge' | 'kingbiglarge' | 
          'colorsmall' | 'colormedium' | 'colorlarge' | 'colorbiglarge' | 'colorkingbiglarge';
   label: string;
   onClick?: () => void;
 }
 
-/** Primary UI component for user interaction */
 const Button = ({
   primary = false,
   size = 'medium',
@@ -18,13 +17,8 @@ const Button = ({
   onClick,
   ...props
 }: ButtonProps) => {
-  // mode는 primary 여부에 따라 className을 동적으로 설정합니다.
   const mode = primary ? styles['storybook-button--primary'] : styles['storybook-button--secondary'];
-
-  // 버튼 크기에 따른 스타일을 적용
   const sizeClass = styles[`storybook-button--${size}`];
-
-  // 최종적으로 합쳐진 className
   const buttonClass = [styles['storybook-button'], sizeClass, mode].join(' ');
 
   return (
@@ -32,7 +26,7 @@ const Button = ({
       type="button"
       className={buttonClass}
       style={{ backgroundColor }}
-      onClick={onClick}  // onClick 이벤트 핸들러 처리
+      onClick={onClick}
       {...props}
     >
       {label}
